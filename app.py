89f22f5ce7921f1908d2786b6b3209cfa7fab0ae
SHRIMATHI R <shrimathiravi44@gmail.com>
2026-06-29 23:51:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
+import os
 import joblib
 import pandas as pd
 from flask import Flask, request, jsonify
 
+# Initialize Flask app
 app = Flask(__name__)
 
 # Load model
 model = joblib.load("logistic_regression_diabetes_model.joblib")
 
 # Load scaler
-scaler = joblib.load("scaler.joblib")
+scalar = joblib.load("scalar.joblib")
 
 # Load feature names
 feature_columns = joblib.load("feature_columns.joblib")
 
 
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Diabetes Prediction API is running successfully!"
+    })
+
+
 @app.route("/predict", methods=["POST"])
 def predict():
-
     try:
-
+        # Get JSON input
         data = request.get_json()
 
+        if not data:
+            return jsonify({"error": "No input data received"}), 400
+
+        # Convert to DataFrame
         input_df = pd.DataFrame([data])
 
         # Add missing columns
@@ -28,27 +40,28 @@ def predict():
             if col not in input_df.columns:
                 input_df[col] = 0
 
-        # Correct order
+        # Arrange columns in training order
         input_df = input_df[feature_columns]
 
-        # Scale
-        scaled = scaler.transform(input_df)
+        # Scale input
+        scaled_data = scalar.transform(input_df)
 
         # Prediction
-        pred = model.predict(scaled)[0]
+        prediction = model.predict(scaled_data)[0]
 
-        proba = model.predict_proba(scaled)[0]
+        # Prediction probability
+        probability = model.predict_proba(scaled_data)[0]
 
         return jsonify({
-            "prediction": int(pred),
-            "probability_no_diabetes": float(proba[0]),
-            "probability_diabetes": float(proba[1])
+            "prediction": int(prediction),
+            "probability_no_diabetes": round(float(probability[0]), 4),
+            "probability_diabetes": round(float(probability[1]), 4)
         })
 
     except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-        return jsonify({"error": str(e)}),400
 
-
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
